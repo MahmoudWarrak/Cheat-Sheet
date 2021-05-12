a1e1c00b028b49eceac0b07532e5cb853f4b3eca
Mahmoud Warrak <47008501+MahmoudWarrak@users.noreply.github.com>
2021-05-12 17:14:24 +0300

    Create Ternary Search.cpp

---
+++ Ternary Search.cpp
@@ -0,0 +1,12 @@
+
+		ll lo = 0 , hi = n-1 , mid ; 
+		ll ans = min( f(lo),f(hi) ) ;
+ 
+        while ( hi-lo > 1 ){
+        mid = (lo+hi)/2 ;
+        ll ans1 = f(mid) , ans2 = f(mid+1) ;
+        if( ans1 > ans2 )
+            lo = mid ,ans = min ( ans , ans2 );
+        else
+            hi = mid ,ans = min ( ans , ans1 );
+        }
