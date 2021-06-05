08548155bf716c93face15ee841160897afa19e8
Mahmoud Warrak <47008501+MahmoudWarrak@users.noreply.github.com>
2021-06-06 00:23:51 +0300

    Update Ternary Search for finding minimum answer.cpp

--- Search Algorithms/Ternary Search/Ternary Search for finding minimum answer.cpp
+++ Search Algorithms/Ternary Search/Ternary Search for finding minimum answer.cpp
@@ -1,12 +1,14 @@
-
+{
 	ll lo = MinVal , hi = MAxVal , mid ; 
 	ll ans = min( f(lo),f(hi) ) ;
  
         while ( hi-lo > 1 ){
-        mid = (lo+hi)/2 ;
-        ll ans1 = f(mid) , ans2 = f(mid+1) ;
-        if( ans1 > ans2 )
-            lo = mid ,ans = min ( ans , ans2 );
-        else
-            hi = mid ,ans = min ( ans , ans1 );
+        	mid = (lo+hi)/2 ;
+        	ll ans1 = f(mid) , ans2 = f(mid+1) ;
+        	if( ans1 > ans2 )
+            		lo = mid ,ans = min ( ans , ans2 );
+        	else
+            		hi = mid ,ans = min ( ans , ans1 );
         }
+	return ans ; 
+}
