2d3356f8c0f8a36140cf2f53a94a64bbb5221b18
Mahmoud Warrak <47008501+MahmoudWarrak@users.noreply.github.com>
2021-06-05 03:47:14 +0300

    Update Ternary Search for finding maximum answer.cpp

--- Search Algorithms/Ternary Search/Ternary Search for finding maximum answer.cpp
+++ Search Algorithms/Ternary Search/Ternary Search for finding maximum answer.cpp
@@ -1,1 +1,15 @@
 
+ld ts ( ld a , ld b , ld c ){
+    ld lo = MinVal , hi = MaxVal , mid ;
+	ld ans = max( f(lo),f(hi) ) ;
+ 
+        while ( hi-lo >= EPS ){
+        mid = (lo+hi)/2 ;
+        ld ans1 = f(mid,b,c) , ans2 = f(mid+EPS,b,c) ;
+        if( ans2 >= ans1 )
+            lo = mid ,ans = max ( ans , ans2 );
+        else
+            hi = mid ,ans = max ( ans , ans1 );
+        }
+        return ans ;
+}
