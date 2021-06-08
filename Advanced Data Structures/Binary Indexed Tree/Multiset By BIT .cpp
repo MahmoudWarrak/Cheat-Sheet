4299924ebde13a46bf2c558e25b341de9b3ef492
Mahmoud Warrak <47008501+MahmoudWarrak@users.noreply.github.com>
2021-06-08 04:30:31 +0300

    Update Multiset By BIT .cpp

--- Advanced Data Structures/Binary Indexed Tree/Multiset By BIT .cpp
+++ Advanced Data Structures/Binary Indexed Tree/Multiset By BIT .cpp
@@ -1,34 +1,7 @@
+// Same Basic BIT notes..
+// You may have to compress the values and decompress them..
+
 const int N = 1<<18 ; // ~= 1e5 ;
-struct BIT
-{
-    int tree[N];
-    void init()
-    {
-        memset(tree, 0, sizeof tree);
-    }
-    void add(int pos, int val)
-    {
-        for(++pos ; pos <= N ; pos += (pos & (-pos))) tree[pos - 1] += val;
-    }
-    int get(int pos)
-    {
-        int ret = 0;
-        for(++pos ; pos ; pos -= (pos & (-pos))) ret += tree[pos - 1];
-        return ret;
-    }
-    int find(int t)
-    {
-        int st = 0;
-        for(int sz = N >> 1 ; sz ; sz >>= 1)
-        {
-            if(tree[st + sz - 1] < t)
-            {
-                t -= tree[(st += sz) - 1];
-            }
-        }
-        return st;
-    }
-};
 
 struct multiSet
 {
@@ -59,4 +32,4 @@ struct multiSet
         return bit.find(ind);
     }
 };
-// Same Basic BIT notes..
+
