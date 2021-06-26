6794ed0ae9681720c09c3db2b4bf2f46b1b2cc1a
Mahmoud Warrak <47008501+MahmoudWarrak@users.noreply.github.com>
2021-06-26 05:04:17 +0300

    Create Linear Sieve.cpp

---
+++ Number Theory/Primes and stuff/Linear Sieve.cpp
@@ -0,0 +1,16 @@
+const int MAX = 2e6 + 10;
+vector <int> prime;
+int IsComposite[MAX];
+
+void LinearSieve (int n) {
+	memset ( IsComposite , 0 , sizeof IsComposite ) ;
+	for ( int i = 2 ; i < n ; ++i ) {
+		if ( !IsComposite[i] )
+            prime.push_back (i) ;
+		for ( int j = 0 ; j < prime.size () && i * prime[j] < n ; ++j ) {
+			IsComposite [i * prime[j]] = i ;
+			if ( !( i % prime[j] ) )
+                break ;
+		}
+	}
+}
