bd7eadf53d1fc14d1bccf710e77e9103fe4da134
Mahmoud Warrak <47008501+MahmoudWarrak@users.noreply.github.com>
2021-11-16 23:53:07 +0200

    Update Slope.cpp

--- Geometry/Slope.cpp
+++ Geometry/Slope.cpp
@@ -9,3 +9,33 @@
     }
     int b = m2*p[i].Y - m1*p[i].X ;
     st.insert({b,{m1,m2}}) ;
+
+
+    ll dx = x2 - x1 ;
+    ll dy = y2 - y11 ;
+    ll gcd = __gcd ( dx , dy ) ;
+    dx /= gcd ;
+    dy /= gcd ;
+    if ( dx < 0 ){
+        dx =- dx ;
+        dy =- dy ;
+    }
+    if ( dx ){
+        ll nx = (x1%dx+dx)%dx ;
+        ll ny = y11+(nx-x1)/dx*dy ;
+        if ( !st.count({dx,dy,nx,ny}) ){
+            ans += mp[{-dy,dx}] ;
+            ans += mp[{dy,-dx}] ;
+            mp[{dx,dy}] ++ ;
+
+            st.insert({dx,dy,nx,ny}) ;
+        }
+    }
+    else{
+        if ( !amodi.count(x1) ){
+            ans += mp[{-dy,dx}] ;
+            ans += mp[{dy,-dx}] ;
+            mp[{dx,dy}]++ ;
+            amodi.insert(x1) ;
+        }
+    }
